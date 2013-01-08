c30cf7a2092e7383a0f0ca865ec512f067316ce6
cwilson14 <cwilson14@10.80.10.28>
2013-01-08 10:00:25 -0700

    Board and Space classes

---
+++ src/Board.java
@@ -0,0 +1,77 @@
+
+public class Board {
+	Space[][] arr;
+	
+	public Board(){
+		for(int i=0;i<15;i++){
+			for(int j=0; j<15;j++){
+				arr[i][j]=new Space(0);
+			}
+		}
+		//all of the triple word spaces
+		arr[0][0]=new Space(4);
+		arr[7][0]=new Space(4);
+		arr[14][0]=new Space(4);
+		arr[0][7]=new Space(4);
+		arr[14][7]=new Space(4);
+		arr[0][14]=new Space(4);
+		arr[7][14]=new Space(4);
+		arr[14][14]=new Space(4);
+		
+		//all of the double word spaces
+		for(int i=1; i<5; i++){
+			for(int j=1;j<5;j++){
+				arr[i][j]=new Space(3);
+			}
+			for(int j=13;j>9;j--){
+				arr[i][j]=new Space(3);
+			}
+		}
+		for(int i=13;i>9;i--){
+			for(int j=1; j<5;j++){
+				arr[i][j]=new Space(3);
+			}
+			for(int j=13;j>11;j--){
+				arr[i][j]=new Space(3);
+			}
+		}
+		//all of the triple letter spaces
+		for(int i=1;i<14;i+=4){
+			for(int j=5;j<10;j+=4){
+				arr[i][j]=new Space(2);
+		}
+		}
+		arr[5][1]=new Space(2);
+		arr[9][1]=new Space(2);
+		arr[5][13]=new Space(2);
+		arr[9][13]=new Space(2);
+		//all of the double letter spaces
+		arr[3][0]=new Space(1);
+		arr[0][3]=new Space(1);
+		arr[6][2]=new Space(1);
+		arr[2][6]=new Space(1);
+		arr[7][3]=new Space(1);
+		arr[3][7]=new Space(1);
+		arr[8][2]=new Space(1);
+		arr[2][8]=new Space(1);
+		arr[11][0]=new Space(1);
+		arr[0][11]=new Space(1);
+		arr[6][6]=new Space(1);
+		arr[8][6]=new Space(1);
+		arr[6][8]=new Space(1);
+		arr[8][8]=new Space(1);
+		arr[14][3]=new Space(1);
+		arr[3][14]=new Space(1);
+		arr[12][6]=new Space(1);
+		arr[6][12]=new Space(1);
+		arr[11][7]=new Space(1);
+		arr[7][11]=new Space(1);
+		arr[12][8]=new Space(1);
+		arr[8][12]=new Space(1);
+		arr[14][11]=new Space(1);
+		arr[11][14]=new Space(1);
+	}
+	
+	
+	
+}
