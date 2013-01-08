eaaf9e2c15f98e9782bd7587f0e22493cd1bc664
MrClement <aclement@kentdenver.org>
2013-01-08 09:51:03 -0700

    Added comments

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,29 +1,47 @@
 import java.util.HashMap;
 import java.util.Scanner;
 
+/**
+ * @author MrClement
+ * 
+ *         The Dictionary class is designed to check whether words are in the
+ *         scrabble dictionary and provide a list of all valid scrabble words
+ * 
+ */
 public class Dictionary {
 
 	private String filename = "dictionary.txt";
-	private HashMap<Integer, String> allWords;
+	private HashMap<String, Integer> allWords;
 
 	public Dictionary() {
-		allWords = new HashMap<Integer, String>();
+		allWords = new HashMap<String, Integer>();
 		Scanner scan = new Scanner(Dictionary.class.getClassLoader().getResourceAsStream(filename));
 		String line = "";
 		Word w;
 		while (scan.hasNext()) {
 			line = scan.nextLine();
 			w = new Word(line);
-			allWords.put(w.getVal(), line);
+			allWords.put(line, w.getVal());
 		}
 	}
 
-	public boolean isWord(String guess) {
+	/**
+	 * isWord takes a word returns its point value if it is a valid word, if it
+	 * is not it return -1
+	 * 
+	 * @param guess
+	 * @return -1 if invalid, point value if valid
+	 */
+	public int isWord(String guess) {
 		guess = guess.toUpperCase();
-		return allWords.containsValue(guess);
+		return allWords.containsKey(guess) ? allWords.get(guess) : -1;
 	}
 
-	public HashMap<Integer, String> getAllWords() {
+	/**
+	 * @return a HashMap of all valid scrabble words (key) and their base values
+	 *         (value)
+	 */
+	public HashMap<String, Integer> getAllWords() {
 		return allWords;
 	}
 
