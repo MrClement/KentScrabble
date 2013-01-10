0151cc8aaaf9b2a8f653662a653821cc4987d141
Preston Mueller <skierjunk@gmail.com>
2013-01-10 08:43:33 -0700

    More work on gui

--- src/core/ScrabbleGUI.java
+++ src/core/ScrabbleGUI.java
@@ -1,11 +1,11 @@
-package core;
 
 import java.awt.Color;
 import java.awt.EventQueue;
-
+import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class ScrabbleGUI {
 
@@ -29,8 +29,40 @@ public class ScrabbleGUI {
 		frame.setSize(900, 900);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
+		
+		showBoard();
 	}
 
+	
+	//Colors
+	
+	
+	private void showBoard() {
+		int i = 16;
+		int j = 16;
+
+		GridLayout layout = new GridLayout(i, j);
+		layout.setHgap(2);
+		layout.setVgap(2);
+
+		JPanel[][] panelHolder = new JPanel[i][j];
+		frame.setLayout(layout);
+		
+		for (int m = 0; m < i; m++) {
+			for (int n = 0; n < j; n++) {
+				panelHolder[m][n] = new JPanel();
+				panelHolder[m][n].setSize(60, 60);
+				/*if ((n % 2 + m % 2) % 2 == 0)
+					panelHolder[m][n].setBackground(Color.WHITE);
+				else
+					panelHolder[m][n].setBackground(Color.GRAY);
+					*/
+				
+				frame.add(panelHolder[m][n]);
+			}
+		}
+		
+	}
 
 	protected ImageIcon createImageIcon(String path, String description) {
 		java.net.URL url = getClass().getResource(path);
