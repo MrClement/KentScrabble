159724e4b76ffa40cbb33f9b396c21d5a9614e62
MrClement <aclement@kentdenver.org>
2013-01-11 15:08:22 -0700

    Made clientConnection with object writing

--- src/server/ClientConnection.java
+++ src/server/ClientConnection.java
@@ -3,6 +3,7 @@ package server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 
@@ -15,6 +16,7 @@ public class ClientConnection implements Runnable {
 	PrintStream out;
 	Board b;
 	ObjectInputStream objIn;
+	ObjectOutputStream objOut;
 
 	public ClientConnection(Socket client) {
 		this.client = client;
@@ -26,6 +28,7 @@ public class ClientConnection implements Runnable {
 		try {
 			objIn = new ObjectInputStream(client.getInputStream());
 			out = new PrintStream(client.getOutputStream());
+			objOut = new ObjectOutputStream(client.getOutputStream());
 			Object next;
 			while ((next = objIn.readObject()) != null) {
 				b = (Board) next;
