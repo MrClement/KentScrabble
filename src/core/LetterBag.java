7b16ec5a3f30b441d8fae4fa803d49aac31ddffb
MrClement <aclement@kentdenver.org>
2013-01-22 08:20:04 -0700

    Imporved draw

--- src/core/LetterBag.java
+++ src/core/LetterBag.java
@@ -1,50 +1,49 @@
 package core;
+
 import java.util.ArrayList;
 import java.util.Random;
 
 /**
  * @author Alex Patel
- *
- * The collection of all the tiles used in a game. 
- * Starts with 100 tiles with the original Scrabble distribution.
+ * 
+ *         The collection of all the tiles used in a game. Starts with 100 tiles
+ *         with the original Scrabble distribution.
  * 
  * 
  */
 public class LetterBag {
 	private ArrayList<Letter> bag;
-	public static int numLetters=100;
-	
-	public LetterBag(){
-		bag=new ArrayList<Letter>();
+	public static int numLetters = 100;
+
+	public LetterBag() {
+		bag = new ArrayList<Letter>();
 		fill();
 	}
-	
-	
+
 	/**
 	 * @return number of tiles in bag
 	 */
-	public int getSize(){
+	public int getSize() {
 		return bag.size();
 	}
 
-	
 	/**
-	 * fills bag with original Scrabble distribution of letters. 
-	 * Randomly fills the bag if more letters requested 
-	 * and randomly takes away letters of low value away if less
+	 * fills bag with original Scrabble distribution of letters. Randomly fills
+	 * the bag if more letters requested and randomly takes away letters of low
+	 * value away if less
 	 */
-	public void fill(){
-		//normal letter distribution
-		for(int i=1;i<13;i++){
+	public void fill() {
+		// normal letter distribution
+		for (int i = 1; i < 13; i++) {
 			bag.add(new Letter('E'));
-			if(i<2){
+			if (i < 2) {
 				bag.add(new Letter('K'));
 				bag.add(new Letter('J'));
 				bag.add(new Letter('X'));
 				bag.add(new Letter('Q'));
 				bag.add(new Letter('Z'));
 			}
-			if(i<3){
+			if (i < 3) {
 				bag.add(new Letter('0'));
 				bag.add(new Letter('B'));
 				bag.add(new Letter('C'));
@@ -56,66 +55,69 @@ public class LetterBag {
 				bag.add(new Letter('W'));
 				bag.add(new Letter('Y'));
 			}
-			if(i<4){
+			if (i < 4) {
 				bag.add(new Letter('G'));
 			}
-			if(i<5){
+			if (i < 5) {
 				bag.add(new Letter('L'));
 				bag.add(new Letter('S'));
 				bag.add(new Letter('U'));
 				bag.add(new Letter('D'));
 			}
-			if(i<7){
+			if (i < 7) {
 				bag.add(new Letter('N'));
 				bag.add(new Letter('R'));
 				bag.add(new Letter('T'));
 			}
-			if(i<9){
+			if (i < 9) {
 				bag.add(new Letter('O'));
 			}
-			if(i<10){
+			if (i < 10) {
 				bag.add(new Letter('A'));
 				bag.add(new Letter('I'));
 			}
 		}
-		
-		//If more than 100 letters wanted in the bag, then add random letters on top of normal distribution
-		if(numLetters>100){
-		for(int i=0;i<numLetters-100;i++){
-			String alph="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-			Random r=new Random();
-			bag.add(new Letter(alph.charAt(r.nextInt(26))));
+
+		// If more than 100 letters wanted in the bag, then add random letters
+		// on top of normal distribution
+		if (numLetters > 100) {
+			for (int i = 0; i < numLetters - 100; i++) {
+				String alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+				Random r = new Random();
+				bag.add(new Letter(alph.charAt(r.nextInt(26))));
+			}
 		}
-		}
-		
-		//If fewer than 100 letters want in the bag, than take out random 1 and 2 point letters until number is reached
-		else if(numLetters<100){
-			for(int i=0;i<100-numLetters;i++){
-				Random r=new Random();
-				int q=r.nextInt(bag.size());
-				while(bag.get(q).getVal()!=1||bag.get(q).getVal()!=2){
-					q=r.nextInt(bag.size());
+
+		// If fewer than 100 letters want in the bag, than take out random 1 and
+		// 2 point letters until number is reached
+		else if (numLetters < 100) {
+			for (int i = 0; i < 100 - numLetters; i++) {
+				Random r = new Random();
+				int q = r.nextInt(bag.size());
+				while (bag.get(q).getVal() != 1 || bag.get(q).getVal() != 2) {
+					q = r.nextInt(bag.size());
 				}
 				bag.remove(q);
 			}
 		}
-		
+
 	}
- 
-	
+
 	/**
-	 * @param how many (random) letters to draw from the bag
+	 * @param how
+	 *            many (random) letters to draw from the bag
 	 * @return ArrayList<Letter> with the requested random pieces
 	 * 
-	 * also removes the pieces from the collection after given to player
+	 *         also removes the pieces from the collection after given to player
 	 */
-	public ArrayList<Letter> draw(int numLetters){
-		ArrayList<Letter> d=new ArrayList<Letter>();
-		Random r=new Random();
-		int j=r.nextInt(bag.size());
-		for(int i=0;i<numLetters;i++){
+	public ArrayList<Letter> draw(int numLetters) {
+		ArrayList<Letter> d = new ArrayList<Letter>();
+		Random r = new Random();
+		int j = r.nextInt(bag.size());
+		for (int i = 0; i < numLetters; i++) {
 			d.add(new Letter(bag.get(j).getCharacter()));
-			bag.remove(j);
+			if (bag.size() > 0)
+				bag.remove(j);
 		}
 		return d;
 	}
--- src/core/Player.java
+++ src/core/Player.java
@@ -1,64 +1,18 @@
 package core;
+
 import java.util.ArrayList;
 
 public abstract class Player {
-	
-	public static final int numLetters=7;
+
+	public static final int numLetters = 7;
 	protected ArrayList<Letter> letters;
 	protected int score;
 	protected LetterBag bag;
-	
-	public Player(LetterBag bag){
+
+	public Player(LetterBag bag) {
 		letters = new ArrayList<Letter>();
 		this.bag = bag;
 		score = 0;
-	}
-	
-	public abstract Word makeMove(Board b);
-	
-	public int getScore(){
-		return score;
-	}
-	
-	public ArrayList<Letter> getLetters(){
-		return letters;
-	}
-	
-	public void draw(int num){
-		for(int i=0;i<num;i++){
-			letters.add(bag.draw(num).get(i));
-		}
-	}
-	
-	public void fill(){
-		while(letters.size()<7||bag.getSize()>0){
-			letters.add(bag.draw(1).get(0));
-		}
-	}
-	
-	public void addScore(int i){
-		score+=i;
-	}
-	
-	
-}
-
-
-/*
-
-import java.util.ArrayList;
-
-public abstract class Player {
-	private int score = 0;
-	private ArrayList<Letter> availableCharacters = new ArrayList<Letter>();
-	private LetterBag letterSack;
-	private int numLetters = 0;
-
-	public Player(LetterBag ls) {
-		letterSack = ls;
-		setNumLetters(7);
-		setAvailableCharacters();
-
 	}
 
 	public abstract Word makeMove(Board b);
@@ -67,31 +21,63 @@ public abstract class Player {
 		return score;
 	}
 
-	public void setScore(int i) {
-		score = i;
+	public ArrayList<Letter> getLetters() {
+		return letters;
 	}
 
-	public void setAvailableCharacters() {
-		availableCharacters = letterSack.draw(getNumLetters());
-		setNumLetters(0);
+	public boolean draw(int num) {
+		ArrayList<Letter> drawn = bag.draw(num);
+		for (int i = 0; i < num; i++) {
+			letters.add(drawn.get(i));
+		}
+		if (drawn.size() == num)
+			return true;
+		else
+			return false;
 	}
 
-	public void incrementScore(int i) {
+	public void fill() {
+		while (letters.size() < 7 || bag.getSize() > 0) {
+			letters.add(bag.draw(1).get(0));
+		}
+	}
+
+	public void addScore(int i) {
 		score += i;
-	}
-
-	public ArrayList<Letter> getAvailableCharacters() {
-		return availableCharacters;
-	}
-
-	public int getNumLetters() {
-		return numLetters;
-	}
-
-	public void setNumLetters(int numLetters) {
-		this.numLetters = numLetters;
 	}
 
 }
 
-*/
+/*
+ * 
+ * import java.util.ArrayList;
+ * 
+ * public abstract class Player { private int score = 0; private
+ * ArrayList<Letter> availableCharacters = new ArrayList<Letter>(); private
+ * LetterBag letterSack; private int numLetters = 0;
+ * 
+ * public Player(LetterBag ls) { letterSack = ls; setNumLetters(7);
+ * setAvailableCharacters();
+ * 
+ * }
+ * 
+ * public abstract Word makeMove(Board b);
+ * 
+ * public int getScore() { return score; }
+ * 
+ * public void setScore(int i) { score = i; }
+ * 
+ * public void setAvailableCharacters() { availableCharacters =
+ * letterSack.draw(getNumLetters()); setNumLetters(0); }
+ * 
+ * public void incrementScore(int i) { score += i; }
+ * 
+ * public ArrayList<Letter> getAvailableCharacters() { return
+ * availableCharacters; }
+ * 
+ * public int getNumLetters() { return numLetters; }
+ * 
+ * public void setNumLetters(int numLetters) { this.numLetters = numLetters; }
+ * 
+ * }
+ */
