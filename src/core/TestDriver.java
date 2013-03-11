96b4e8ce565dae13a946fb5ebb8660eca0a6a72e
MrClement <aclement@kentdenver.org>
2013-03-11 11:23:33 -0600

    Fixed one of word's constructors.

--- src/core/TestDriver.java
+++ src/core/TestDriver.java
@@ -18,5 +18,20 @@ public class TestDriver {
 		System.out.println(temp);
 		System.out.println(d.allStringsWithLetters(temp));
 
+		Letter l1 = new Letter('k');
+		Letter l2 = new Letter('k');
+		System.out.println(l1);
+		System.out.println(l2);
+		System.out.println(l1.hashCode());
+		System.out.println(l2.hashCode());
+		System.out.println(l1.equals(l2));
+		ArrayList<Letter> test = new ArrayList<Letter>();
+		test.add(l1);
+		test.add(l2);
+		System.out.println(test);
+		test.remove(new Letter('k'));
+		System.out.println(test);
+		test.remove(new Letter('k'));
+		System.out.println(test);
 	}
 }
